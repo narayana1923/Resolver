5fcce2f1f9508267ed057e9df92e78c9d18049bd
Lakshmi Narayana Velayudam <lcchinnu@gmail.com>
2022-09-18 22:38:47 +0530

    added select queries for db

---
+++ selectQueries.sql
@@ -0,0 +1,37 @@
+-- Employee queries
+SELECT 
+    *
+FROM
+    employee;
+
+-- Project queries
+SELECT 
+    *
+FROM
+    project;
+
+-- Ticket queries
+SELECT 
+    *
+FROM
+    ticket;
+
+SELECT 
+    *
+FROM
+    ticket
+WHERE
+    tid = 37;
+
+-- Ticket details queries
+SELECT 
+    *
+FROM
+    ticketdetails;
+
+SELECT 
+    *
+FROM
+    ticketdetails
+WHERE
+    tid = 37;
